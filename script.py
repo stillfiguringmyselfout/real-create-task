631abd992c0c4619fa9f11bae44630153e692607
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-04-23 15:59:35 +0000

    Commit 1st Day Test

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
+def define(dict, s, msg1, msg2):
 
+  g = "yes"
+  
+  while g == "yes":
+    word = input(s)
+    if dictionary.get(word, 0) != 0:
+      print(msg1, word, msg2, dictionary[word] + ".")
+      g = input("Search again? yes/no ")
+    else:
+      print("ERROR")
