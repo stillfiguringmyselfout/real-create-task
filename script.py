411e51fd5c58cf48ff1a43bb53d295a75f67f7f0
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-04-24 19:46:52 +0000

    fixed formatting issues

--- script.py
+++ script.py
@@ -1,4 +1,13 @@
-vocab_words = {"mundane":"commonplace and often boring", "retrogress":"to return to an earlier and usually worse condition; to move or travel backward", "incipient":"beginning to appear or develop", "perfidy":"treachery or deceit", "covert":"not intended to be seen, known, or found out", "descry":"to catch sight of something", "transitory":"not permanent or lasting, but existing only for a short time", "hoax":"an act intended to trick people into believing something is real when it is not", "penury":"extreme poverty", "stigma":"the shame or disgrace attached to something regarded as socially unacceptable"}
+vocab_words = {"mundane":"commonplace and often boring", 
+"retrogress":"to return to an earlier and usually worse condition; to move or travel backward", 
+"incipient":"beginning to appear or develop", 
+"perfidy":"treachery or deceit", 
+"covert":"not intended to be seen, known, or found out", 
+"descry":"to catch sight of something", 
+"transitory":"not permanent or lasting, but existing only for a short time", 
+"hoax":"an act intended to trick people into believing something is real when it is not", 
+"penury":"extreme poverty", 
+"stigma":"the shame or disgrace attached to something regarded as socially unacceptable"}
 
 def define(dict, s, msg1, msg2):
 
